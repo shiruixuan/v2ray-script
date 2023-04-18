7f2020a56f582457c3db0473107806c72fd9c0ad
shiruixuan <101796849+shiruixuan@users.noreply.github.com>
2023-04-18 11:13:15 +0800

    Update v2ray-script.sh

--- v2ray-script.sh
+++ v2ray-script.sh
@@ -4,18 +4,18 @@
 
 
 mkdir -p ~/nginx/conf.d
-mkdir -p ~/nginx/sslcert
+mkdir -p ~/nginx/cert
 mkdir -p ~/v2ray
 
 docker run --rm -it -v ~/acme.sh:/acme.sh --net=host neilpang/acme.sh --set-default-ca --server letsencrypt
 docker run --rm -it -v ~/acme.sh:/acme.sh --net=host neilpang/acme.sh --issue -d bjjtw.top --keylength ec-256 --standalone
-docker run --rm -it -v ~/acme.sh:/acme.sh -v ~/nginx/sslcert:/etc/nginx/sslcert --net=host neilpang/acme.sh --install-cert -d bjjtw.top --ecc --key-file /etc/nginx/sslcert/bjjtw.top.key --fullchain-file /etc/nginx/sslcert/bjjtw.top.pem
+docker run --rm -it -v ~/acme.sh:/acme.sh -v ~/nginx/cert:/etc/nginx/cert --net=host neilpang/acme.sh --install-cert -d bjjtw.top --ecc --key-file /etc/nginx/cert/bjjtw.top.key --fullchain-file /etc/nginx/cert/bjjtw.top.pem
 
-echo "0 0 * * * docker run --rm -it --net=host -v ~/acme.sh:/acme.sh -v ~/nginx/sslcert:/etc/nginx/sslcert neilpang/acme.sh --cron > /dev/null" >> /var/spool/cron/crontabs/root
+echo "0 0 * * * docker run --rm -it --net=host -v ~/acme.sh:/acme.sh -v ~/nginx/cert:/etc/nginx/cert neilpang/acme.sh --cron > /dev/null" >> /var/spool/cron/crontabs/root
 
 wget https://raw.githubusercontent.com/shiruixuan/v2ray-script/main/nginx.conf -O ~/nginx/nginx.conf
 wget https://raw.githubusercontent.com/shiruixuan/v2ray-script/main/bjjtw.top.conf -O ~/nginx/conf.d/bjjtw.top.conf
-docker run -d --net=host --name=nginx --restart=always -v ~/nginx/nginx.conf:/etc/nginx/nginx.conf -v ~/nginx/conf.d:/etc/nginx/conf.d -v ~/nginx/sslcert:/etc/nginx/sslcert nginx
+docker run -d --net=host --name=nginx --restart=always -v ~/nginx/nginx.conf:/etc/nginx/nginx.conf -v ~/nginx/conf.d:/etc/nginx/conf.d -v ~/nginx/cert:/etc/nginx/cert nginx
 
 bash <(curl -fsSL git.io/warp.sh) proxy
 
